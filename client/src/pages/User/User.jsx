5970f90a8c609e4ab0be4b33556bab81cbc8e1c0
Kevin <kevincordier.dev@gmail.com>
2026-04-28 16:00:30 +0200

    refactor(user): restructure edit name form JSX

--- client/src/pages/User/User.jsx
+++ client/src/pages/User/User.jsx
@@ -6,37 +6,54 @@ function User() {
 
     return (
         <main className="main bg-dark">
-            {!isOpen ? (
-                <div className="header">
-                    <h1>
-                        Welcome back
-                        <br />
-                        Tony Jarvis!
-                    </h1>
-                    <button
-                        className="edit-button"
-                        onClick={() => setIsOpen(true)}
-                    >
-                        Edit Name
-                    </button>
-                </div>
-            ) : (
-                <div className="header">
-                    <h1>Welcome back</h1>
-                    <form>
-                        {" "}
-                        {/*onSubmit={}*/}
-                        <div className="inputs-wrapper">
-                            <input type="text" id="firstname" placeholder="Tony"/>
-                            <input type="text" id="lastname" placeholder="Jarvis"/>
-                        </div>
-                        <div className="buttons-wrapper">
-                            <button className="edit-name-btn" type="submit">Save</button>
-                             <button className="edit-name-btn" type="button" onClick={() => setIsOpen(false)}>Cancel</button>
-                        </div>
-                    </form>
-                </div>
-            )}
+            <div className="header">
+                {!isOpen ? (
+                    <>
+                        <h1>
+                            Welcome back
+                            <br />
+                            Tony Jarvis!
+                        </h1>
+                        <button
+                            className="edit-button"
+                            onClick={() => setIsOpen(true)}
+                        >
+                            Edit Name
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <h1>Welcome back</h1>
+                        <form>
+                            {/*onSubmit={}*/}
+                            <div className="inputs-wrapper">
+                                <input
+                                    type="text"
+                                    id="firstname"
+                                    placeholder="Tony"
+                                />
+                                <input
+                                    type="text"
+                                    id="lastname"
+                                    placeholder="Jarvis"
+                                />
+                            </div>
+                            <div className="buttons-wrapper">
+                                <button className="edit-name-btn" type="submit">
+                                    Save
+                                </button>
+                                <button
+                                    className="edit-name-btn"
+                                    type="button"
+                                    onClick={() => setIsOpen(false)}
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </>
+                )}
+            </div>
 
             <h2 className="sr-only">Accounts</h2>
             <section className="account">
